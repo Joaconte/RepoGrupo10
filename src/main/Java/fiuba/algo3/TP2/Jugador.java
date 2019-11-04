74bae28f0a3b5147588571c26813a07459a0f06d
Gisse88 <coronel.dg@gmail.com>
2019-11-04 20:59:09 -0300

    Pruebo testJugador

--- src/main/Java/fiuba/algo3/TP2/Jugador.java
+++ src/main/Java/fiuba/algo3/TP2/Jugador.java
@@ -1,4 +1,18 @@
 package fiuba.algo3.TP2;
 
 public class Jugador {
+    static final int PUNTOS_INICIALES = 20;
+    private int puntos;
+
+    public Jugador(){
+        puntos = PUNTOS_INICIALES;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void descontarPuntos(int i) {
+        puntos-=i;
+    }
 }
