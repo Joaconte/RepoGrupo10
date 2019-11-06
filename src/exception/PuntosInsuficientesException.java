005ca880d3b7b9cb622d93c18befd82d9bfc8fd2
Joaconte <43884898+Joaconte@users.noreply.github.com>
2019-11-06 16:34:36 -0300

    Delete PuntosInsuficientesException.java

--- src/exception/PuntosInsuficientesException.java
+++
@@ -1,8 +0,0 @@
-package exception;
-
-public class PuntosInsuficientesException extends Exception{
-
-    public PuntosInsuficientesException(){
-        super("No te alcanzan los puntos esta unidad");
-    }
-}
