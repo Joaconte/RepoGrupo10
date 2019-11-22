dcb5b482ec4c0dd40ed26e2153237c417c6a3e3d
Joaconte <43884898+Joaconte@users.noreply.github.com>
2019-11-22 14:57:28 -0300

    busquedaDeCasillasRecursivasYCambiosDeObject[]aList

--- test/java/tablero/TableroTest.java
+++ test/java/tablero/TableroTest.java
@@ -11,6 +11,9 @@ import org.junit.Test;
 
 import org.mockito.Mockito;
 import pieza.Pieza;
+import tablero.casilla.Casilla;
+
+import java.util.List;
 
 
 public class TableroTest {
@@ -69,7 +72,7 @@ public class TableroTest {
         tablero.ocuparCasilla(pieza, 6,7);
         tablero.ocuparCasilla(pieza, 8,8);
 
-        assertEquals(6,tablero.getPiezasAdyacentes(pieza.getUbicacion()).length);
+        assertEquals(6,tablero.getPiezasAdyacentes(pieza.getUbicacion()).size());
     }
 
     @Test
@@ -89,7 +92,7 @@ public class TableroTest {
         tablero.ocuparCasilla(pieza, 0,0);
         tablero.ocuparCasilla(pieza, 2,2);
 
-        assertEquals(8,tablero.getPiezasAdyacentes(pieza.getUbicacion()).length);
+        assertEquals(8,tablero.getPiezasAdyacentes(pieza.getUbicacion()).size());
     }
 
     @Test
@@ -103,7 +106,7 @@ public class TableroTest {
         tablero.ocuparCasilla(pieza, 1,1);
         tablero.ocuparCasilla(pieza, 0,1);
 
-        assertEquals(2,tablero.getPiezasAdyacentes(pieza.getUbicacion()).length);
+        assertEquals(2,tablero.getPiezasAdyacentes(pieza.getUbicacion()).size());
     }
 
 
@@ -117,8 +120,8 @@ public class TableroTest {
 
         tablero.ocuparCasilla(pieza, 2,2);
 
-        Object[] casillas = tablero.getCasillasVaciasAdyacentes( pieza.getUbicacion() );
-        assertEquals(8, casillas.length);
+        List<Casilla> casillas = tablero.getCasillasVaciasAdyacentes( pieza.getUbicacion() );
+        assertEquals(8, casillas.size());
     }
 
     @Test
@@ -132,9 +135,67 @@ public class TableroTest {
 
         tablero.ocuparCasilla(pieza, 0, 0);
 
-        assertEquals(3, tablero.getCasillasVaciasAdyacentes( pieza.getUbicacion() ).length );
+        assertEquals(3, tablero.getCasillasVaciasAdyacentes( pieza.getUbicacion() ).size() );
     }
 
+    @Test
+    public void test11TableroNoHaceBucleInfinitoAlBuscarPiezasDeFormaRecursivaYEncuentra4(){
+
+        Tablero tablero = new Tablero();
+
+        Jinete pieza1 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza1.getUbicacion()).thenReturn(new Ubicacion(0,0));
+
+        Jinete pieza2 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza2.getUbicacion()).thenReturn(new Ubicacion(1,0));
+
+        Jinete pieza3 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza3.getUbicacion()).thenReturn(new Ubicacion(0,1));
+
+        Jinete pieza4 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza4.getUbicacion()).thenReturn(new Ubicacion(1,1));
+
+        tablero.ocuparCasilla(pieza1, 0, 0);
+        tablero.ocuparCasilla(pieza2, 1, 0);
+        tablero.ocuparCasilla(pieza3, 0, 1);
+        tablero.ocuparCasilla(pieza4, 1, 1);
+
+        assertEquals(4, tablero.getPiezasAdycentesInfinitas( pieza1.getUbicacion() ).size() );
+    }
+
+    @Test
+    public void test12TableroBuscaPiezasDeFormaRecursivaYEncuentra8(){
+
+        Tablero tablero = new Tablero();
+
+        Jinete pieza1 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza1.getUbicacion()).thenReturn(new Ubicacion(0,0));
+        Jinete pieza2 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza2.getUbicacion()).thenReturn(new Ubicacion(1,0));
+        Jinete pieza3 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza3.getUbicacion()).thenReturn(new Ubicacion(2,0));
+        Jinete pieza4 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza4.getUbicacion()).thenReturn(new Ubicacion(3,0));
+        Jinete pieza5 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza5.getUbicacion()).thenReturn(new Ubicacion(4,0));
+        Jinete pieza6 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza6.getUbicacion()).thenReturn(new Ubicacion(4,1));
+        Jinete pieza7 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza7.getUbicacion()).thenReturn(new Ubicacion(4,2));
+        Jinete pieza8 = Mockito.mock(Jinete.class);
+        Mockito.when(pieza8.getUbicacion()).thenReturn(new Ubicacion(5,2));
+
+        tablero.ocuparCasilla(pieza1, 0, 0);
+        tablero.ocuparCasilla(pieza2, 1, 0);
+        tablero.ocuparCasilla(pieza3, 2, 0);
+        tablero.ocuparCasilla(pieza4, 3, 0);
+        tablero.ocuparCasilla(pieza5, 4, 0);
+        tablero.ocuparCasilla(pieza6, 4, 1);
+        tablero.ocuparCasilla(pieza7, 4, 2);
+        tablero.ocuparCasilla(pieza8, 5, 2);
+
+        assertEquals(8, tablero.getPiezasAdycentesInfinitas( pieza1.getUbicacion() ).size() );
+    }
 
 }
 
