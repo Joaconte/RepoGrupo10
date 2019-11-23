6befec277a57190ff3654250a4e2890e08129ec1
Joel <joeljpita91@gmail.com>
2019-11-23 14:09:58 -0300

    CatapultaTest con constructor de piezas reemplazado. + 1 test simple

--- test/java/pieza/CatapultaTest.java
+++ test/java/pieza/CatapultaTest.java
@@ -10,26 +10,28 @@ import pieza.tipos.Catapulta;
 import static org.junit.Assert.assertEquals;
 
 public class CatapultaTest {
+
+    @Test
+    public void test00CatapultaDeEjercito1DaBienSuNumeroDeEjercito() {
+        Catapulta catapulta = new Catapulta(1,2,2);
+        assertEquals(1, catapulta.getEquipo());
+    }
+
     @Test
     public void test01CatapultaCuesta5Puntos() {
-        Catapulta catapulta = new Catapulta(1);
+        Catapulta catapulta = new Catapulta(1,1,1);
         assertEquals(5, catapulta.getCosto());
     }
 
     @Test
     public void test02CatapultaNuevaTiene50PuntosDeVida() {
-        Catapulta catapulta = new Catapulta(1);
-        assertEquals(50, catapulta.getPuntosVida(),0.05);
+        Catapulta catapulta = new Catapulta(1,2,2);
+        assertEquals(50, catapulta.getPuntosVida(),0);
     }
 
     @Test(expected = NoSePuedeMoverException.class)
     public void test03CatapultaNoSePuedeMoverArriba() throws NoSePuedeMoverException {
-        Catapulta catapulta = new Catapulta(1);
-        Ubicacion ubicacion = Mockito.mock(Ubicacion.class);
-        Mockito.when(ubicacion.getPosicionEnY()).thenReturn(1);
-        Mockito.when(ubicacion.getPosicionEnX()).thenReturn(1);
-        catapulta.setUbicacion(ubicacion);
+        Catapulta catapulta = new Catapulta(1,1,1);;
         catapulta.moverArriba();
-        assertEquals(1,catapulta.getUbicacion().getPosicionEnX());
     }
 }
