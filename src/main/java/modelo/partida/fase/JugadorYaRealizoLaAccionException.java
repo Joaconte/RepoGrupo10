a77bfe8c95855f6aad235e733418e5c7d49a349a
Joel <joeljpita91@gmail.com>
2019-12-04 21:43:12 -0300

    edito texto de exception

--- src/main/java/modelo/partida/fase/JugadorYaRealizoLaAccionException.java
+++ src/main/java/modelo/partida/fase/JugadorYaRealizoLaAccionException.java
@@ -2,7 +2,7 @@ package modelo.partida.fase;
 
 public class JugadorYaRealizoLaAccionException extends Exception {
     public  JugadorYaRealizoLaAccionException(){
-        super("Jugador no puede volver a hacer la accion en un mismo turno.");
+        super("Accion de turno gastada.");
     }
 
 }
